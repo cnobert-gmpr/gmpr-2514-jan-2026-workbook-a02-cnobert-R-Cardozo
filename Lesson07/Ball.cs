2a30486cdd1de1c1bc6239af465653d646ef51d7
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-02-11 08:26:02 -0700

    maint - make Initialize a constructor

--- Lesson07/Ball.cs
+++ Lesson07/Ball.cs
@@ -1,14 +1,18 @@
+using System.Formats.Tar;
 using System.Numerics;
 
 namespace Lesson07;
 
 public class Ball
 {
-    private Vector2 _position, _direction;
+    private Vector2 _position, _dimension, _direction;
     private float _speed;
 
     internal void Initialize(Vector2 position, float speed, Vector2 dimensions, Vector2 direction)
     {
-        
+        _position = position;
+        _dimensions = dimensions;
+        _direction = direction;
+        _speed = speed;
     }
 }
