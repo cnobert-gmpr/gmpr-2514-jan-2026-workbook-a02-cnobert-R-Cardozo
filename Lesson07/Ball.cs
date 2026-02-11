cdd2fbc1721d929b75610f9091ca489d0ed66193
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-02-11 08:24:18 -0700

    maint - pass parameters into the Initialize function

--- Lesson07/Ball.cs
+++ Lesson07/Ball.cs
@@ -7,7 +7,7 @@ public class Ball
     private Vector2 _position, _direction;
     private float _speed;
 
-    internal void Initialize()
+    internal void Initialize(Vector2 position, float speed, Vector2 dimensions, Vector2 direction)
     {
         
     }
