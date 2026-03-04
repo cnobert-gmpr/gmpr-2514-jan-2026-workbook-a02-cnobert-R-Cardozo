e1fa19bf27073e414a5bdf203bbe17c4940e0f84
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-04 08:39:17 -0700

    maint - add variables

--- Lesson08/Cannon.cs
+++ Lesson08/Cannon.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+using System.Numerics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -6,4 +8,6 @@ namespace Lesson08;
 public class Cannon
 {
     private SimpleAnimation _animation;
+    private Vector2 _position;
+    private Point _dimensions;
 }
