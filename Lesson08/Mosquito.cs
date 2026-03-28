c59934e44e5baef8a9aa383419ecb3b715f64a61
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-28 17:17:02 -0600

    maint - update mosquito.cs to match in-class example

--- Lesson08/Mosquito.cs
+++ Lesson08/Mosquito.cs
@@ -1,6 +1,7 @@
 using System.IO.Pipes;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using System.Threading.Channels;
 using Microsoft.Win32;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -11,10 +12,13 @@ namespace Lesson08;
 
 public class Mosquito
 {
-    private SimpleAnimation _animation;
+    private SimpleAnimation _animationAlive, _animationPoofing;
     private Vector2 _position, _direction;
     private float _speed;
     private Rectangle _gameBoundingBox;
+
+    private enum State { Alive, Poofing, Dead };
+    private State _state;
 
     private Rectangle BoundingBox
     {
@@ -23,11 +27,13 @@ public class Mosquito
             return new Rectangle(
                 (int)_position.X,
                 (int)_position.Y,
-                (int)_animation.FrameDimensions.X,
-                (int)_animation.FrameDimensions.Y
+                (int)_animationAlive.FrameDimensions.X,
+                (int)_animationAlive.FrameDimensions.Y
             );
         }
     }
+
+    internal bool Alive { get => _state == State.Alive; }
 
     internal void Initialize(Vector2 position, float speed, Vector2 direction, Rectangle gameBoundingBox)
     {
@@ -35,31 +41,74 @@ public class Mosquito
         _speed = speed;
         _direction = direction;
         _gameBoundingBox = gameBoundingBox;
+        _state = State.Alive;
     }
 
     internal void LoadContent(ContentManager content)
     {
         Texture2D texture = content.Load<Texture2D>("Mosquito");
 
-        _animation = new SimpleAnimation(texture, texture.Width / 11, texture.Height, 11, 8f);
-        _animation.Paused = false;
+        _animationAlive = new SimpleAnimation(texture, texture.Width / 11, texture.Height, 11, 8f);
+        _animationAlive.Paused = false;
+
+        texture = content.Load<Texture2D>("Poof");
+        _animationPoofing = new SimpleAnimation(texture, texture.Width / 8, texture.Height, 8, 4);
     }
 
     internal void Update(GameTime gameTime)
     {
         float dt = (float) gameTime.ElapsedGameTime.TotalSeconds;
-        _position += _direction * _speed * dt;
 
-        if(BoundingBox.Left < _gameBoundingBox.Left || BoundingBox.Right > _gameBoundingBox.Right)
+        switch (_state)
         {
-            _direction.X *= -1;
-        }
+            case State.Alive:
+                _position += _direction * _speed * dt;
 
-        _animation.Update(gameTime);
+                if(BoundingBox.Left < _gameBoundingBox.Left || BoundingBox.Right > _gameBoundingBox.Right)
+                {
+                    _direction.X *= -1;
+                }
+
+                _animationAlive.Update(gameTime);
+                break;
+
+            case State.Poofing:
+            _animationPoofing.Update(gameTime);
+
+                if (_animationPoofing.DonePlayingOnce)
+                {
+                    _state = State.Dead;
+                }
+                break;
+
+            case State.Dead:
+                break;
+        }
     }
 
     internal void Draw(SpriteBatch spriteBatch)
     {
-        _animation.Draw(spriteBatch, _position, SpriteEffects.None);
+        switch (_state)
+        {
+            case State.Alive:
+            _animationAlive.Draw(spriteBatch, _position, SpriteEffects.None);
+            break;
+
+            case State.Poofing:
+                _animationPoofing.Draw(spriteBatch, _position, SpriteEffects.None);
+                break;
+
+            case State.Dead:
+                break;
+        }
+    }
+
+    internal void Die()
+    {
+        if (Alive)
+        {
+            _state = State.Poofing;
+            _animationPoofing.Looping = false;
+        }
     }
 }
