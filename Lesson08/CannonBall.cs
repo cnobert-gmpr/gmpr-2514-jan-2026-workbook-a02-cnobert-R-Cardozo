94481ce4104ca2b959f891f7ab48ec5d0f3cfde1
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-03-28 17:08:13 -0600

    maint - update cannonball.cs to match in-class code

--- Lesson08/CannonBall.cs
+++ Lesson08/CannonBall.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+using System.Numerics;
 using Microsoft.Win32;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -11,6 +13,11 @@ public class CannonBall{
     private Vector2 _position, _direction;
     private float _speed;
     private Rectangle _gameBoundingBox;
+
+    private List<Vector2> _trailPositions;
+    private float _trailTimer;
+    private const float _TrailSpawnInterval = 0.1f;
+    private const int _MaxTrailPositions = 8;
 
     private enum State { Flying, NotFlying };
     private State _state = State.NotFlying;
@@ -28,6 +35,9 @@ public class CannonBall{
         _direction = Vector2.Zero;
         _speed = speed;
         _gameBoundingBox = gameBoundingBox;
+
+        _trailPositions = new list<Vector2>();
+        _trailTimer = 0;
     }
 
     internal void LoadContent(ContentManager content)
@@ -44,9 +54,22 @@ public class CannonBall{
         {
             case State.Flying:
                 _position += _direction * _speed * dt;
+
+                _trailTimer += dt;
+                if(_trailTimer >= _TrailSpawnInterval)
+                {
+                    _trailTimer = 0;
+                    _trailPositions.Insert(0, _position);
+                    if(_trailPositions.Count > _MaxTrailPositions)
+                    {
+                        _trailPositions.RemoveAt(_trailPositions.Count - 1);
+                    }
+                }
+
                 if (!BoundingBox.Intersects(_gameBoundingBox))
                 {
                     _state = State.NotFlying;
+                    _trailPositions.Clear();
                 }
                 break;
             case State.NotFlying:
@@ -63,11 +86,35 @@ public class CannonBall{
         {
             case State.Flying:
                 spriteBatch.Draw(_texture, _position, Color.White);
+                DrawTrail(spriteBatch);
                 break;
             case State.NotFlying:
                 break;
         }
         #endregion
+    }
+
+    private void DrawTrail(SpriteBatch spriteBatch)
+    {
+        for(int c = 0; c < _trailPositions.Count; c++)
+        {
+            float alpha = 1f = ((float)(c + 1) / (_trailPositions.Count + 1));
+
+            float scale = 1f - (c * 0.1f);
+            if(scale < 0.2f)
+            {
+                scale = 0.2f;
+            }
+
+            Vector2 drawPosition = _trailPositions[c];
+            Vector2 origin = new Vector2(_texture.Width / 2, _texture.Height / 2);
+            Vector2 centeredPosition = drawPosition + new Vector2(_texture.Width / 2f, _texture.Height / 2f);
+
+            spriteBatch.Draw
+                (
+                    _texture, centeredPosition, null, Color.Gray * (alpha * 0.5f), 0f, origin, scale, SpriteEffects.None, 0f
+                );
+        }
     }
 
     internal void Launch(Vector2 position, Vector2 direction)
@@ -78,5 +125,16 @@ public class CannonBall{
             _direction = direction;
             _state = State.Flying;
         }
+    }
+
+    internal bool ProcessCollision(Rectangle otherBoundingBox)
+    {
+        if(_state = State.Flying && BoundingBox.Intersects(otherBoundingBox))
+        {
+            _state = State.NotFlying;
+            _trailPositions.Clear();
+            return true;
+        }
+        return false;
     }
 }
