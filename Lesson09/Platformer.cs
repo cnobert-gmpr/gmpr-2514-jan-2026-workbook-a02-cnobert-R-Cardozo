03865b3488c77ca31ac40f9e29c3b4716207a6a2
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-04-16 10:06:51 -0600

    maint - rename Game1 to Platformer

--- Lesson09/Platformer.cs
+++ Lesson09/Platformer.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Lesson09;
 
-public class Game1 : Game
+public class Platformer : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public Platformer()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
--- Lesson09/Program.cs
+++ Lesson09/Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new Lesson09.Game1();
+﻿using var game = new Lesson09.Platformer();
 game.Run();
