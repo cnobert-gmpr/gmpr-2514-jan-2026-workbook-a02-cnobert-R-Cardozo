88a514ac8903056ef8c8dbba5390dfdc03d79299
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-04-22 08:30:46 -0600

    maint - add collider and BoundingBox

--- Lesson09/Collider.cs
+++ Lesson09/Collider.cs
@@ -8,5 +8,19 @@ public class Collider
     public enum ColliderType { Top, Right, Bottom, Left }
     private ColliderType _type;
 
-    
+    private Vector2 _position, _dimensions;
+    private Texture2D _pixel;
+    internal Rectangle BoundingBox
+    {
+        get
+        {
+            return new Rectangle((int)_position.X, (int)_position.Y, (int)_dimensions.X, (int)_dimensions.Y);
+        }
+    }
+    public Collider(Vector2 position, Vector2 dimensions, ColliderType colliderType)
+    {
+        _position = position;
+        _dimensions = dimensions;
+        _type = colliderType;
+    }
 }
