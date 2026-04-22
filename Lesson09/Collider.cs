eee7e38d2a24ce51c58b7652a4dd2d5b45c1715b
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-04-22 08:34:11 -0600

    maint - update LoadContent method

--- Lesson09/Collider.cs
+++ Lesson09/Collider.cs
@@ -10,6 +10,7 @@ public class Collider
 
     private Vector2 _position, _dimensions;
     private Texture2D _pixel;
+
     internal Rectangle BoundingBox
     {
         get
@@ -17,10 +18,37 @@ public class Collider
             return new Rectangle((int)_position.X, (int)_position.Y, (int)_dimensions.X, (int)_dimensions.Y);
         }
     }
+
     public Collider(Vector2 position, Vector2 dimensions, ColliderType colliderType)
     {
         _position = position;
         _dimensions = dimensions;
         _type = colliderType;
+    }
+
+    internal void LoadContent(GraphicsDevice graphissDevice)
+    {
+        Color myColour = Color.White;
+        switch(_type)
+        {
+            case ColliderType.Left:
+                myColour = Color.Red;
+                break;
+            case ColliderType.Top:
+                myColour = Color.Blue;
+                break;
+            case ColliderType.Right:
+                myColour = Color.Wheat;
+                break;
+            case ColliderType.Bottom:
+                myColour = Color.Purple;
+                break;
+        }
+        
+        if(_pixel == null)
+        {
+            _pixel = new Texture2D(graphicsDevice, 1, 1);
+            _pixel.SetData(new[] { myColour });
+        }
     }
 }
