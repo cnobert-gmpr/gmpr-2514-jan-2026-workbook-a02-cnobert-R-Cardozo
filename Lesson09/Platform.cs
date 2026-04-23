180d460a8c33ed0433ad711aff6fa2c41537439c
Rachael Cardozo <Rachael.Cardozo@gmail.com>
2026-04-23 10:21:09 -0600

    maint - add LoadContent, Draw, and ProcessCollisions methods

--- Lesson09/Platform.cs
+++ Lesson09/Platform.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using System.Runtime.CompilerServices;
+using System.Security;
 
 namespace Lesson09;
 
@@ -21,5 +22,23 @@ public class Platform
         _colliders[1] = new Collider(new Vector2(position.X + dimensions.X - 1, position.Y + 1), new Vector2(1, dimensions.Y - 2), ColliderType.Right);
         _colliders[2] = new Collider(new Vector2(position.X + 3, position.Y + dimensions.Y), new Vector2(dimensions.X - 6, 1), ColliderType.Bottom);
         _colliders[3] = new Collider(new Vector2(position.X + 1, position.Y + 1), new Vector2(1, dimensions.Y - 2), ColliderType.Left);
+    }
+
+    internal void LoadContent(GraphicsDevice graphicsDevice)
+    {
+        foreach(Collider c in _colliders)
+            c.LoadContent(graphicsDevice);
+    }
+
+    internal void Draw(SpriteBatch spriteBatch)
+    {
+        foreach(Collider c in _colliders)
+            c.Draw(spriteBatch);
+    }
+
+    internal void ProcessCollisions(Player player, GameTime gameTime)
+    {
+        foreach(Collider c in _colliders)
+            c.ProcessCollision(player, gameTime);
     }
 }
